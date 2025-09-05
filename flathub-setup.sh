a94fdd8641f64df3a98c752e9169d0b659fb351d
elserandom101 <pramodvu1502@proton.me>
2025-09-05 11:57:16 +0530

    Yet another fix in flathub-setup.sh

--- flathub-setup.sh
+++ flathub-setup.sh
@@ -4,14 +4,14 @@ set -xeuo pipefail
 # Add flathub repo
 flatpak --system remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 
-# Reinstall programs already installed through flathub
-flatpak --system install -y --reinstall flathub $(flatpak list --app-runtime=org.fedoraproject.Platform --columns=application | tail -n +1 )
-
 # Prioritize flathub
 flatpak remote-modify --system --prio=1 flathub ||:
 
 # Deprioritize fedora-flatpaks
 flatpak remote-modify --system --prio=2 fedora ||:
+
+# Reinstall programs already installed through flathub
+flatpak --system install -y flathub --reinstall $(flatpak list --app-runtime=org.fedoraproject.Platform --columns=application | tail -n +1 )
 
 # Add some useful things like codecs etc...
 RUNEXTS=(
@@ -25,6 +25,6 @@ flatpak --system install -y flathub "${RUNEXTS[@]}"
 DEFPROG=(
   "org.mozilla.firefox"
 )
-flatpak -y --system install flathub "${DEFPROG[@]}"
+flatpak --system install -y flathub "${DEFPROG[@]}"
 
 exit 0
