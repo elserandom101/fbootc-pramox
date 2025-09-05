75b3275f8bb51248cb96cde947a4ff9818f239b0
elserandom101 <pramodvu1502@proton.me>
2025-09-05 11:40:40 +0530

    Correct command flags in flathub-setup.sh

--- flathub-setup.sh
+++ flathub-setup.sh
@@ -2,16 +2,16 @@
 set -xeuo pipefail
 
 # Add flathub repo
-flatpak -y --system remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+flatpak --system remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 
 # Reinstall programs already installed through flathub
-flatpak -y --system install --reinstall flathub $(flatpak list --app-runtime=org.fedoraproject.Platform --columns=application | tail -n +1 )
+flatpak --system install -y --reinstall flathub $(flatpak list --app-runtime=org.fedoraproject.Platform --columns=application | tail -n +1 )
 
 # Prioritize flathub
-flatpak -y remote-modify --system --prio=1 flathub
+flatpak remote-modify --system --prio=1 flathub ||:
 
 # Deprioritize fedora-flatpaks
-flatpak -y remote-modify --system --prio=2 fedora ||:
+flatpak remote-modify --system --prio=2 fedora ||:
 
 # Add some useful things like codecs etc...
 RUNEXTS=(
@@ -19,7 +19,7 @@ RUNEXTS=(
   "org.freedesktop.Platform.VulkanLayer"
   "org.freedesktop.Platform.codecs-extra"
 )
-flatpak -y --system install flathub "${RUNEXTS[@]}"
+flatpak --system install -y flathub "${RUNEXTS[@]}"
 
 # Replace some default included applications
 DEFPROG=(
